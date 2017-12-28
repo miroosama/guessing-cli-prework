191ca364c509af27cfbc8b2f1d4da9276f1f5a59
osama miro <o.miro650@gmail.com>
2017-12-28 02:33:47 +0000

    automatically backed up by learn

--- guessing_cli.rb
+++ guessing_cli.rb
@@ -3,7 +3,7 @@ def run_guessing_game
   guess = gets.chomp
   puts "Guess a number between 1 and 6."
   if guess == (1..6)
-    "You guessed the correct number!"
+    puts "You guessed the correct number!"
   else
     puts "Goodbye!"
   end
